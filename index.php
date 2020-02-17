3707c53c7dc2517af850d234a6e753da03c77cf7
Dmitrijs G <u1909586@live.warwick.ac.uk>
2020-02-17 16:42:22 +0000

    Spelling

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php require 'header.php'; ?>
   <main>
-    <h1>Main Page</h1>
     <?php
       if (isset($_SESSION['userID'])) {
         require 'logged_in.php';
--- logged_out.php
+++ logged_out.php
@@ -3,5 +3,5 @@
   <h3>Here you can add expenses that have to be settled by a group of people</h3>
   <p>Make it easy for yourself, others will get an email and website notifications of repayments. It's easy to use, just
   add people with their email addresses and the money they owe. Once they have paid you will be updated on the information!!</p>
-  <h5>Simple a that!</h5>
+  <h4>Simple as that!</h4>
 </main>
