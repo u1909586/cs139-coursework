46ceb0efcbca771341fae6039cdf9303ae59edc1
Dmitrijs G <u1909586@live.warwick.ac.uk>
2020-02-19 13:22:50 +0000

    expenses correctly added

--- add_expense.inc.php
+++ add_expense.inc.php
@@ -9,9 +9,14 @@ $stmt->bindValue(':userID', $userID, SQLITE3_INTEGER);
 $stmt->bindValue(':name', $refernce, SQLITE3_TEXT);
 $stmt->bindValue(':date_now', $date, SQLITE3_TEXT);
 $results = $stmt->execute();
+$stmt = $db->prepare("SELECT ExpenseID FROM Expenses ORDER BY ExpenseID DESC LIMIT 1");
+$result = $stmt->execute();
+while ($row = $result->fetchArray()) {
+  $expenseID = "{$row['ExpenseID']}";
+}
 for ($i=1; $i <= count($array) ; $i++) {
   $stmt = $db->prepare("INSERT INTO ExpenseOwe(ExpenseID, UserID, Name, Amount, Reference, Email, Paid) Values(:expenseID, :userID, :name, :amount, :reference, :email, FALSE)");
-  $stmt->bindValue(':expenseID', 0, SQLITE3_INTEGER); // Change the expense ID to the correct expense ID;
+  $stmt->bindValue(':expenseID', $expenseID, SQLITE3_INTEGER); // Change the expense ID to the correct expense ID;
   $stmt->bindValue(':userID', $_SESSION['userID'], SQLITE3_INTEGER);
   $stmt->bindValue(':name', $array[$i]['name'], SQLITE3_TEXT);
   $stmt->bindValue(':amount',  $array[$i]['amount'], SQLITE3_INTEGER);
